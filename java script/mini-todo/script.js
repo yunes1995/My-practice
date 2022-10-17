a3f548fdb95f262e10dbd0d863d16720df5394e0
yunes_mohammadi <88889070+yunes1995@users.noreply.github.com>
2022-10-17 13:33:55 +0330

    Create script.js

---
+++ java script/mini-todo/script.js
@@ -0,0 +1,15 @@
+
+
+
+function AddNewTask() {
+let myDiv = document.getElementById("contetntid");
+let newElem = document.createElement('div');
+newElem.className = "newDiv";
+newElem.innerHTML = "<p>Test</p>";
+myDiv.appendChild(newElem);
+}
+
+function test(element , className) {
+let newelem = document.createElement(element);
+
+}
